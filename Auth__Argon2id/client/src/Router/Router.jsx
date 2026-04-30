b2c4c5cbe792acd339b08cf286d1ec4a565c4ace
ART4578 <artur17-00@inbox.ru>
2026-04-30 14:02:02 +0400

    Web__Apps__React__Node__MySQL

--- Auth__Argon2id/client/src/Router/Router.jsx
+++ Auth__Argon2id/client/src/Router/Router.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "../Pages/Register/Register";
 import Login from "../Pages/Login/Login";
 import Home from "../Pages/Home/Home";
@@ -7,17 +7,19 @@ import ProtectedRoute from "../ProtectedRoute/ProtectedRoute";
 
 function Router() {
     return (
-        <Routes>
-            <Route path="/" element={
-                <ProtectedRoute>
-                    <Home/>
-                </ProtectedRoute>
-                }
-            />
-            <Route path="/login" element={<Login/>} />
-            <Route path="/register" element={<Register/>} />
-            <Route path="*" element={<NotFound/>} />
-        </Routes>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={
+                    <ProtectedRoute>
+                        <Home/>
+                    </ProtectedRoute>
+                    }
+                />
+                <Route path="/login" element={<Login/>} />
+                <Route path="/register" element={<Register/>} />
+                <Route path="*" element={<NotFound/>} />
+            </Routes>
+        </BrowserRouter>
     );
 };
 
