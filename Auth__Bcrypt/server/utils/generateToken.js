dea5fed56289acca0d92741e09e6b5ddc03e4d13
ART4578 <artur17-00@inbox.ru>
2026-05-08 11:41:46 +0400

    Web__Apps__React__Node__MySQL

--- Auth__Bcrypt/server/utils/generateToken.js
+++
@@ -1,8 +0,0 @@
-import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-export const generateToken = (user) => {
-    return jwt.sign(user, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES });
-};
